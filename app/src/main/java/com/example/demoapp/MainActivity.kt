e1872687556eb62c7786ee8a9f8e4f8df0d4ec50
shubhanVats <svats5443@gmail.com>
2021-08-08 16:39:26 +0530

    add Model class

--- app/src/main/java/com/example/demoapp/MainActivity.kt
+++ app/src/main/java/com/example/demoapp/MainActivity.kt
@@ -2,12 +2,14 @@ package com.example.demoapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.demoapp.R.layout
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(layout.activity_main)
     }
 }
---
+++ app/src/main/java/com/example/demoapp/data/model/network/ColorResponse.kt
@@ -0,0 +1,3 @@
+package com.example.demoapp.data.model.network
+
+class ColorResponse : ArrayList<ColorResponseItem>()
